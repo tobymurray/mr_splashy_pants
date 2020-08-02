42a2bb7b222e27a7f795690b9d9d7bc9f515fd4d
Toby Murray <toby.murray@protonmail.com>
2020-08-01 23:43:34 -0400

    Add some starter structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+    }
+}
+
+use reqwest::Client;
+
+pub struct Pants {
+    client: Client,
+    client_configuration: ClientConfiguration,
+}
+
+impl Pants {
+    pub fn new(user_agent: &str, access_token: &str) -> Pants {
+        Pants {
+            client: Client::new(),
+            client_configuration: ClientConfiguration::new(user_agent, access_token),
+        }
+    }
+}
+
+pub struct ClientConfiguration {
+    user_agent: String,
+    access_token: String,
+}
+
+impl ClientConfiguration {
+    pub fn new(user_agent: &str, access_token: &str) -> ClientConfiguration {
+        ClientConfiguration {
+            user_agent: user_agent.to_string(),
+            access_token: access_token.to_string(),
+        }
     }
 }
