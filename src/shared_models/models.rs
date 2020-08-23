1b7c9d728b000230e82ff7e98bd1e0bf10a04580
Toby Murray <toby.murray@protonmail.com>
2020-08-23 11:51:30 -0400

    Add root structure for listing endpoints

--- src/shared_models/models.rs
+++ src/shared_models/models.rs
@@ -15,3 +15,27 @@ impl ClientConfiguration {
         }
     }
 }
+
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Listing<T> {
+    data: ListingData<T>,
+    kind: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListingData<T> {
+    pub after: String,
+    pub before: ::serde_json::Value,
+    pub children: Vec<Children<T>>,
+    pub dist: i64,
+    pub modhash: ::serde_json::Value,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Children<T> {
+    pub data: T,
+    pub kind: String,
+}
