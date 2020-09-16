0ce4ba044ae3560a9e6d60e6defbad24642c6103
Toby Murray <toby.murray@protonmail.com>
2020-09-16 19:55:11 -0400

    Fixes #6: Make all properties in /{{subreddit}}/new response optional
    Reddit doesn't provide a response schema. The most robust approach is to consider every property in the response optional. While this lowers the usability a bit, as presumably there are fields that are required (e.g. ID?), it prevents catastrophic runtime errors.

--- src/api/generated/response/listing/subreddit_new.rs
+++ src/api/generated/response/listing/subreddit_new.rs
@@ -4,179 +4,179 @@ use std::fmt;
 #[serde(rename_all = "camelCase")]
 pub struct Data {
     #[serde(rename = "all_awardings")]
-    pub all_awardings: Vec<::serde_json::Value>,
+    pub all_awardings: Option<Vec<::serde_json::Value>>,
     #[serde(rename = "allow_live_comments")]
-    pub allow_live_comments: bool,
+    pub allow_live_comments: Option<bool>,
     #[serde(rename = "approved_at_utc")]
-    pub approved_at_utc: ::serde_json::Value,
+    pub approved_at_utc: Option<::serde_json::Value>,
     #[serde(rename = "approved_by")]
-    pub approved_by: ::serde_json::Value,
-    pub archived: bool,
-    pub author: String,
+    pub approved_by: Option<::serde_json::Value>,
+    pub archived: Option<bool>,
+    pub author: Option<String>,
     #[serde(rename = "author_flair_background_color")]
-    pub author_flair_background_color: ::serde_json::Value,
+    pub author_flair_background_color: Option<::serde_json::Value>,
     #[serde(rename = "author_flair_css_class")]
-    pub author_flair_css_class: ::serde_json::Value,
+    pub author_flair_css_class: Option<::serde_json::Value>,
     #[serde(rename = "author_flair_richtext")]
-    pub author_flair_richtext: Vec<::serde_json::Value>,
+    pub author_flair_richtext: Option<Vec<::serde_json::Value>>,
     #[serde(rename = "author_flair_template_id")]
-    pub author_flair_template_id: ::serde_json::Value,
+    pub author_flair_template_id: Option<::serde_json::Value>,
     #[serde(rename = "author_flair_text")]
-    pub author_flair_text: ::serde_json::Value,
+    pub author_flair_text: Option<::serde_json::Value>,
     #[serde(rename = "author_flair_text_color")]
-    pub author_flair_text_color: ::serde_json::Value,
+    pub author_flair_text_color: Option<::serde_json::Value>,
     #[serde(rename = "author_flair_type")]
-    pub author_flair_type: String,
+    pub author_flair_type: Option<String>,
     #[serde(rename = "author_fullname")]
-    pub author_fullname: String,
+    pub author_fullname: Option<String>,
     #[serde(rename = "author_patreon_flair")]
-    pub author_patreon_flair: bool,
+    pub author_patreon_flair: Option<bool>,
     #[serde(rename = "author_premium")]
-    pub author_premium: bool,
-    pub awarders: Vec<::serde_json::Value>,
+    pub author_premium: Option<bool>,
+    pub awarders: Option<Vec<::serde_json::Value>>,
     #[serde(rename = "banned_at_utc")]
-    pub banned_at_utc: ::serde_json::Value,
+    pub banned_at_utc: Option<::serde_json::Value>,
     #[serde(rename = "banned_by")]
-    pub banned_by: ::serde_json::Value,
+    pub banned_by: Option<::serde_json::Value>,
     #[serde(rename = "can_gild")]
-    pub can_gild: bool,
+    pub can_gild: Option<bool>,
     #[serde(rename = "can_mod_post")]
-    pub can_mod_post: bool,
-    pub category: ::serde_json::Value,
-    pub clicked: bool,
+    pub can_mod_post: Option<bool>,
+    pub category: Option<::serde_json::Value>,
+    pub clicked: Option<bool>,
     #[serde(rename = "content_categories")]
-    pub content_categories: ::serde_json::Value,
+    pub content_categories: Option<::serde_json::Value>,
     #[serde(rename = "contest_mode")]
-    pub contest_mode: bool,
-    pub created: f64,
+    pub contest_mode: Option<bool>,
+    pub created: Option<f64>,
     #[serde(rename = "created_utc")]
-    pub created_utc: f64,
+    pub created_utc: Option<f64>,
     #[serde(rename = "discussion_type")]
-    pub discussion_type: ::serde_json::Value,
-    pub distinguished: ::serde_json::Value,
-    pub domain: String,
-    pub downs: i64,
-    pub edited: ::serde_json::Value,
-    pub gilded: i64,
-    pub gildings: Gildings,
-    pub hidden: bool,
+    pub discussion_type: Option<::serde_json::Value>,
+    pub distinguished: Option<::serde_json::Value>,
+    pub domain: Option<String>,
+    pub downs: Option<i64>,
+    pub edited: Option<::serde_json::Value>,
+    pub gilded: Option<i64>,
+    pub gildings: Option<Gildings>,
+    pub hidden: Option<bool>,
     #[serde(rename = "hide_score")]
-    pub hide_score: bool,
-    pub id: String,
+    pub hide_score: Option<bool>,
+    pub id: Option<String>,
     #[serde(rename = "is_crosspostable")]
-    pub is_crosspostable: bool,
+    pub is_crosspostable: Option<bool>,
     #[serde(rename = "is_meta")]
-    pub is_meta: bool,
+    pub is_meta: Option<bool>,
     #[serde(rename = "is_original_content")]
-    pub is_original_content: bool,
+    pub is_original_content: Option<bool>,
     #[serde(rename = "is_reddit_media_domain")]
-    pub is_reddit_media_domain: bool,
+    pub is_reddit_media_domain: Option<bool>,
     #[serde(rename = "is_robot_indexable")]
-    pub is_robot_indexable: bool,
+    pub is_robot_indexable: Option<bool>,
     #[serde(rename = "is_self")]
-    pub is_self: bool,
+    pub is_self: Option<bool>,
     #[serde(rename = "is_video")]
-    pub is_video: bool,
-    pub likes: ::serde_json::Value,
+    pub is_video: Option<bool>,
+    pub likes: Option<::serde_json::Value>,
     #[serde(rename = "link_flair_background_color")]
-    pub link_flair_background_color: String,
+    pub link_flair_background_color: Option<String>,
     #[serde(rename = "link_flair_css_class")]
-    pub link_flair_css_class: ::serde_json::Value,
+    pub link_flair_css_class: Option<::serde_json::Value>,
     #[serde(rename = "link_flair_richtext")]
-    pub link_flair_richtext: Vec<::serde_json::Value>,
+    pub link_flair_richtext: Option<Vec<::serde_json::Value>>,
     #[serde(rename = "link_flair_text")]
-    pub link_flair_text: ::serde_json::Value,
+    pub link_flair_text: Option<::serde_json::Value>,
     #[serde(rename = "link_flair_text_color")]
-    pub link_flair_text_color: String,
+    pub link_flair_text_color: Option<String>,
     #[serde(rename = "link_flair_type")]
-    pub link_flair_type: String,
-    pub locked: bool,
-    pub media: ::serde_json::Value,
+    pub link_flair_type: Option<String>,
+    pub locked: Option<bool>,
+    pub media: Option<::serde_json::Value>,
     #[serde(rename = "media_embed")]
-    pub media_embed: MediaEmbed,
+    pub media_embed: Option<MediaEmbed>,
     #[serde(rename = "media_only")]
-    pub media_only: bool,
+    pub media_only: Option<bool>,
     #[serde(rename = "mod_note")]
-    pub mod_note: ::serde_json::Value,
+    pub mod_note: Option<::serde_json::Value>,
     #[serde(rename = "mod_reason_by")]
-    pub mod_reason_by: ::serde_json::Value,
+    pub mod_reason_by: Option<::serde_json::Value>,
     #[serde(rename = "mod_reason_title")]
-    pub mod_reason_title: ::serde_json::Value,
+    pub mod_reason_title: Option<::serde_json::Value>,
     #[serde(rename = "mod_reports")]
-    pub mod_reports: Vec<::serde_json::Value>,
-    pub name: String,
+    pub mod_reports: Option<Vec<::serde_json::Value>>,
+    pub name: Option<String>,
     #[serde(rename = "no_follow")]
-    pub no_follow: bool,
+    pub no_follow: Option<bool>,
     #[serde(rename = "num_comments")]
-    pub num_comments: i64,
+    pub num_comments: Option<i64>,
     #[serde(rename = "num_crossposts")]
-    pub num_crossposts: i64,
+    pub num_crossposts: Option<i64>,
     #[serde(rename = "num_reports")]
-    pub num_reports: ::serde_json::Value,
+    pub num_reports: Option<::serde_json::Value>,
     #[serde(rename = "over_18")]
-    pub over18: bool,
+    pub over18: Option<bool>,
     #[serde(rename = "parent_whitelist_status")]
-    pub parent_whitelist_status: String,
-    pub permalink: String,
-    pub pinned: bool,
-    pub pwls: i64,
-    pub quarantine: bool,
+    pub parent_whitelist_status: Option<String>,
+    pub permalink: Option<String>,
+    pub pinned: Option<bool>,
+    pub pwls: Option<i64>,
+    pub quarantine: Option<bool>,
     #[serde(rename = "removal_reason")]
-    pub removal_reason: ::serde_json::Value,
+    pub removal_reason: Option<::serde_json::Value>,
     #[serde(rename = "removed_by")]
-    pub removed_by: ::serde_json::Value,
+    pub removed_by: Option<::serde_json::Value>,
     #[serde(rename = "removed_by_category")]
-    pub removed_by_category: ::serde_json::Value,
+    pub removed_by_category: Option<::serde_json::Value>,
     #[serde(rename = "report_reasons")]
-    pub report_reasons: ::serde_json::Value,
-    pub saved: bool,
-    pub score: i64,
+    pub report_reasons: Option<::serde_json::Value>,
+    pub saved: Option<bool>,
+    pub score: Option<i64>,
     #[serde(rename = "secure_media")]
-    pub secure_media: ::serde_json::Value,
+    pub secure_media: Option<::serde_json::Value>,
     #[serde(rename = "secure_media_embed")]
-    pub secure_media_embed: SecureMediaEmbed,
-    pub selftext: String,
+    pub secure_media_embed: Option<SecureMediaEmbed>,
+    pub selftext: Option<String>,
     #[serde(rename = "selftext_html")]
     pub selftext_html: Option<String>,
     #[serde(rename = "send_replies")]
-    pub send_replies: bool,
-    pub spoiler: bool,
-    pub stickied: bool,
-    pub subreddit: String,
+    pub send_replies: Option<bool>,
+    pub spoiler: Option<bool>,
+    pub stickied: Option<bool>,
+    pub subreddit: Option<String>,
     #[serde(rename = "subreddit_id")]
-    pub subreddit_id: String,
+    pub subreddit_id: Option<String>,
     #[serde(rename = "subreddit_name_prefixed")]
-    pub subreddit_name_prefixed: String,
+    pub subreddit_name_prefixed: Option<String>,
     #[serde(rename = "subreddit_subscribers")]
-    pub subreddit_subscribers: i64,
+    pub subreddit_subscribers: Option<i64>,
     #[serde(rename = "subreddit_type")]
-    pub subreddit_type: String,
+    pub subreddit_type: Option<String>,
     #[serde(rename = "suggested_sort")]
-    pub suggested_sort: ::serde_json::Value,
-    pub thumbnail: String,
+    pub suggested_sort: Option<::serde_json::Value>,
+    pub thumbnail: Option<String>,
     #[serde(rename = "thumbnail_height")]
     pub thumbnail_height: Option<i64>,
     #[serde(rename = "thumbnail_width")]
     pub thumbnail_width: Option<i64>,
-    pub title: String,
+    pub title: Option<String>,
     #[serde(rename = "top_awarded_type")]
-    pub top_awarded_type: ::serde_json::Value,
+    pub top_awarded_type: Option<::serde_json::Value>,
     #[serde(rename = "total_awards_received")]
-    pub total_awards_received: i64,
+    pub total_awards_received: Option<i64>,
     #[serde(rename = "treatment_tags")]
-    pub treatment_tags: Vec<::serde_json::Value>,
-    pub ups: i64,
+    pub treatment_tags: Option<Vec<::serde_json::Value>>,
+    pub ups: Option<i64>,
     #[serde(rename = "upvote_ratio")]
-    pub upvote_ratio: f64,
-    pub url: String,
+    pub upvote_ratio: Option<f64>,
+    pub url: Option<String>,
     #[serde(rename = "user_reports")]
-    pub user_reports: Vec<::serde_json::Value>,
+    pub user_reports: Option<Vec<::serde_json::Value>>,
     #[serde(rename = "view_count")]
-    pub view_count: ::serde_json::Value,
-    pub visited: bool,
+    pub view_count: Option<::serde_json::Value>,
+    pub visited: Option<bool>,
     #[serde(rename = "whitelist_status")]
-    pub whitelist_status: String,
-    pub wls: i64,
+    pub whitelist_status: Option<String>,
+    pub wls: Option<i64>,
     #[serde(rename = "post_hint")]
     pub post_hint: Option<String>,
     pub preview: Option<Preview>,
@@ -186,7 +186,12 @@ pub struct Data {
 
 impl fmt::Display for Data {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.id, self.title)
+        write!(
+            f,
+            "({}, {})",
+            self.id.as_ref().unwrap_or(&"[NO ID]".to_string()),
+            self.title.as_ref().unwrap_or(&"[NO TITLE]".to_string())
+        )
     }
 }
 
@@ -205,33 +210,33 @@ pub struct SecureMediaEmbed {}
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Preview {
-    pub enabled: bool,
-    pub images: Vec<Image>,
+    pub enabled: Option<bool>,
+    pub images: Option<Vec<Image>>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Image {
-    pub id: String,
-    pub resolutions: Vec<Resolution>,
-    pub source: Source,
-    pub variants: Variants,
+    pub id: Option<String>,
+    pub resolutions: Option<Vec<Resolution>>,
+    pub source: Option<Source>,
+    pub variants: Option<Variants>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Resolution {
-    pub height: i64,
-    pub url: String,
-    pub width: i64,
+    pub height: Option<i64>,
+    pub url: Option<String>,
+    pub width: Option<i64>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Source {
-    pub height: i64,
-    pub url: String,
-    pub width: i64,
+    pub height: Option<i64>,
+    pub url: Option<String>,
+    pub width: Option<i64>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
@@ -244,45 +249,45 @@ pub struct Variants {
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Nsfw {
-    pub resolutions: Vec<Resolution2>,
-    pub source: Source2,
+    pub resolutions: Option<Vec<Resolution2>>,
+    pub source: Option<Source2>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Resolution2 {
-    pub height: i64,
-    pub url: String,
-    pub width: i64,
+    pub height: Option<i64>,
+    pub url: Option<String>,
+    pub width: Option<i64>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Source2 {
-    pub height: i64,
-    pub url: String,
-    pub width: i64,
+    pub height: Option<i64>,
+    pub url: Option<String>,
+    pub width: Option<i64>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Obfuscated {
-    pub resolutions: Vec<Resolution3>,
-    pub source: Source3,
+    pub resolutions: Option<Vec<Resolution3>>,
+    pub source: Option<Source3>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Resolution3 {
-    pub height: i64,
-    pub url: String,
-    pub width: i64,
+    pub height: Option<i64>,
+    pub url: Option<String>,
+    pub width: Option<i64>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Source3 {
-    pub height: i64,
-    pub url: String,
-    pub width: i64,
+    pub height: Option<i64>,
+    pub url: Option<String>,
+    pub width: Option<i64>,
 }
